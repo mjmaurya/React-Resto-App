805ac8da86976200154a31affb536676f7454c2a
Manoj Kumar <gcssm1999@gmail.com>
2021-05-06 17:29:52 +0530

    Responsive navBar added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,15 +1,44 @@
 import React,{Component} from 'react'
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Nav,Navbar, NavbarBrand,NavbarToggler,Collapse,NavItem, Jumbotron } from 'reactstrap';
+import {NavLink} from 'react-router-dom'
 
 class Header extends Component{
+constructor(props){
+    super(props);
+    this.state={
+        isOpenTogler:false
+    }
+    this.ToggleNav=this.ToggleNav.bind(this);
 
-
+}
+ToggleNav(){
+    this.setState({
+        isOpenTogler:!this.state.isOpenTogler
+    });
+}
     render(){
         return(
             <React.Fragment>
-                <Navbar dark color="primary">
+                <Navbar dark expand="md">
                     <div className="container">
+                    <NavbarToggler onClick={this.ToggleNav}/>
                     <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    <Collapse isOpen={this.state.isOpenTogler} navbar>
+                    <Nav navbar>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/home"><span>Home</span></NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/aboutus"><span>About Us</span></NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/contactus"><span>Contact Us</span></NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className="nav-link" to="/menu"><span>Menu</span></NavLink>
+                        </NavItem>
+                    </Nav>
+                    </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,30 +1,34 @@
 import React,{Component} from 'react'
 import { DISHES } from '../shared/dishes';
-import MenuComponent from './MenuComponent';
-import DishdetailComponent from './DishdetailComponent'
+import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './Home';
+import {Switch,Route,Redirect} from 'react-router-dom'
 class Main extends Component {
   constructor(props){
     super(props);
 
     this.state={
-      dishes:DISHES,
-      selectedDish:null
+      dishes:DISHES
     }
   }
-  dishSelected(dishId){
-    this.setState({
-        selectedDish:dishId
-    });
-    
-}
+   
   render(){
+    const HomePage=()=>{
+      return(
+        <Home/>
+      );
+    }
+    
     return (
       <div className="App">
         <Header/>
-          <MenuComponent dishes={this.state.dishes} onClick={(dishId)=>this.dishSelected(dishId)}/>
-          <DishdetailComponent selectedDish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}/>
+        <Switch>
+          <Route path="/home" component={HomePage}/>
+          <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
+          <Redirect to="/home"/>
+        </Switch>
         <Footer/>
       </div>
     );
